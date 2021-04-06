6b3f45d0e81c9d8ddac30ba220cab881114f384c
Diana-nyamai <dnyamai.dn@gmail.com>
2021-04-06 17:58:37 +0300

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
 import './App.css';
-import Navbar from './components/Navigators/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+    hello
     </div>
   );
 }
