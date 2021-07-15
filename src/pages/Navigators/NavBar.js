0cae6f21a75bbe4a5c4beb7a638b06d489187ca6
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-15 16:33:37 +0300

    update

--- src/pages/Navigators/NavBar.js
+++ src/pages/Navigators/NavBar.js
@@ -10,9 +10,10 @@ function NavBar() {
         <Header style={{backgroundColor:'white'}}>
           <div className="logo" />
           <Menu theme="white" mode="horizontal" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="1">Home</Menu.Item>
+          <Menu.Item key="2">Properties</Menu.Item>
+          <Menu.Item key="3">Pages</Menu.Item>
+          <Menu.Item key="3">Shop</Menu.Item>
           </Menu>
         </Header>
         
