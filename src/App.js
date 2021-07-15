13dba56db3f69faa5f3a7182b404e5523580960d
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-15 17:27:19 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './pages/Navigators/NavBar';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
     <NavBar/>
+
     </div>
   );
 }
