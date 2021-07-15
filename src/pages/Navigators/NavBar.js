6571376ecd9d8b13e4d7dcc4777c58a4af5dffe5
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-15 16:54:59 +0300

    update

--- src/pages/Navigators/NavBar.js
+++ src/pages/Navigators/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './NavBar.css';
 import { Layout, Menu} from 'antd';
 import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import { Row } from 'react-bootstrap';
 
 const { Header } = Layout;
 
@@ -9,6 +10,7 @@ function NavBar() {
     return (
         <Layout className="layout">
         <Header style={{backgroundColor:'white'}}>
+        <Row>
           <div className="logo" />
           <Menu theme="white" mode="horizontal" defaultSelectedKeys={['1']}>
           <Menu.Item key="1">HOME</Menu.Item>
@@ -16,11 +18,7 @@ function NavBar() {
           <Menu.Item key="3">PAGES</Menu.Item>
           <Menu.Item key="4">SHOP</Menu.Item>
           </Menu>
-
-          <Menu mode="horizontal">
-          <Menu.Item key="5">SHOP</Menu.Item>
-          <Menu.Item key="6">SHOP</Menu.Item>
-          </Menu>
+     </Row>
         </Header>
         
       </Layout>
