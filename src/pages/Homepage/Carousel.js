6a8aaa716117a8bdcd86dac123e33f34d906521d
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-15 17:20:51 +0300

    update

--- src/pages/Homepage/Carousel.js
+++ src/pages/Homepage/Carousel.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import { Carousel } from 'antd';
 
+const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+  
+
 function Carousel() {
     return (
         <div>
