9e02ae390853204f10a2495a49f0eeeaaad11d0f
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-15 17:48:46 +0300

    update

--- src/pages/Homepage/CarouselHome.js
+++ src/pages/Homepage/CarouselHome.js
@@ -1,35 +1,30 @@
 import React from 'react'
 import { Carousel } from 'antd';
 import BH from '../../assets/images/bhMain.png'
-import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles(() =>({
-     contentStyle: {
-          height: '500px',
-          color: '#000',
-          lineHeight: '400px',
-          textAlign: 'center',
-          background: '#fff',
-  },
-}))
- 
+const contentStyle = {
+    height: '500px',
+    color: '#000',
+    lineHeight: '400px',
+    textAlign: 'center',
+    background: '#fff',
+  };
   
 
 function CarouselHome() {
-  const Classes = useStyles();
     return (
         <Carousel autoplay>
     <div>
-      <img style={Classes.contentStyle} src={BH} alt="caro"/>
+      <img style={contentStyle} src={BH} alt="caro"/>
     </div>
     <div>
-    <img style={Classes.contentStyle} src={BH} alt="caro"/>
+    <img style={contentStyle} src={BH} alt="caro"/>
     </div>
     <div>
-    <img style={Classes.contentStyle} src={BH} alt="caro"/>
+    <img style={contentStyle} src={BH} alt="caro"/>
     </div>
     <div>
-    <img style={Classes.contentStyle} src={BH} alt="caro"/>
+      <img style={contentStyle} src={BH} alt="caro"/>
     </div>
   </Carousel>
     )
