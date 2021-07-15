1ba081b57dc3f0f6ddaa8a65dff628043baf7e85
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-15 17:21:16 +0300

    date

--- src/pages/Homepage/Carousel.js
+++ src/pages/Homepage/Carousel.js
@@ -12,9 +12,20 @@ const contentStyle = {
 
 function Carousel() {
     return (
-        <div>
-            
-        </div>
+        <Carousel autoplay>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>up
     )
 }
 
