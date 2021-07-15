d879930474db17e57576ac9f694ceb2f918f511e
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-15 16:14:54 +0300

    update

--- src/pages/Navigators/NavBar.js
+++ src/pages/Navigators/NavBar.js
@@ -15,15 +15,7 @@ function NavBar() {
           <Menu.Item key="3">nav 3</Menu.Item>
           </Menu>
         </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className="site-layout-content">Content</div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}> &copy; {new Date().getFullYear()} Created by Ant UED</Footer>
+        
       </Layout>
     )
 }
