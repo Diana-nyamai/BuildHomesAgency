2251b2d40c59ba3eecd735e4956a4fa7acedee5d
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-29 12:28:08 +0300

    update

--- src/pages/Homepage/Introduction.js
+++ src/pages/Homepage/Introduction.js
@@ -6,7 +6,7 @@ function Introduction() {
         <div style={{padding: 50}}>
             <Grid container>
                <Grid xs={12} sm={6}>
-                    <h1 style={{maxWidth: 100}}>Architecture with people in mind.</h1>
+                    <h1 style={{maxWidth: 400}}>Architecture with people in mind.</h1>
                </Grid>
                <Grid xs={12} sm={6}>
                
