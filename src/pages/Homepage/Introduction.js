1d557c9b48cd83c89ce00f5faaaa138191a42f56
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-29 12:04:41 +0300

    update

--- src/pages/Homepage/Introduction.js
+++ src/pages/Homepage/Introduction.js
@@ -6,6 +6,7 @@ function Introduction() {
         <div style={{padding: 50}}>
             <Grid container>
                <Grid xs={12} sm={6}></Grid>
+               <Grid xs={12} sm={6}></Grid>
             </Grid>
         </div>
     )
