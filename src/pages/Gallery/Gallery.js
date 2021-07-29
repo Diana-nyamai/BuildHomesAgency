0c4afc116310e03f8aaff5232727c18a8bff6ebf
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-29 10:51:26 +0300

    update

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import { BackTop } from 'antd';
 
+const style = {
+    height: 40,
+    width: 40,
+    lineHeight: '40px',
+    borderRadius: 4,
+    backgroundColor: '#1088e9',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 14,
+  };
 function Gallery() {
     return (
         <div>
