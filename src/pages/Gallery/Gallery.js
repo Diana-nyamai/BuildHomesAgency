f8a89440701c94d92ccc6530f7285862d3cc0d13
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-29 15:59:12 +0300

    update

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Introduction from '../Homepage/Introduction'
+
 
 
 function Gallery() {
     return (
         <div>
           <h1>OUR GALLERY</h1>
-          <Introduction/>
+         
    
   </div>
     )
