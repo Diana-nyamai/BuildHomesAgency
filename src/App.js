92eac777d862d136ae8e7e29c1d1fbc95b3ddba3
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-29 11:01:19 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import {BrowserRouter as Router,Route} from 'react-router-dom'
 import HomePage from './pages/Homepage/HomePage';
 import { BackTop } from 'antd';
 
+const style = {
+  height: 40,
+  width: 40,
+  lineHeight: '40px',
+  borderRadius: 4,
+  backgroundColor: 'red',
+  color: '#fff',
+  textAlign: 'center',
+  fontSize: 14,
+};
+
 function App() {
   return (
     <Router>
--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,16 +1,6 @@
 import React from 'react'
 
 
-const style = {
-    height: 40,
-    width: 40,
-    lineHeight: '40px',
-    borderRadius: 4,
-    backgroundColor: 'red',
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: 14,
-  };
 function Gallery() {
     return (
         <div style={{ height: '600vh', padding: 8 }}>
