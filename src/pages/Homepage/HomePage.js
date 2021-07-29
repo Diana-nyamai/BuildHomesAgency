d3c187b86f1ca09226d1ff7f932768b79451900e
Diana-nyamai <dnyamai.dn@gmail.com>
2021-07-29 15:59:45 +0300

    update

--- src/pages/Homepage/HomePage.js
+++ src/pages/Homepage/HomePage.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import AboutPage from '../Aboutpage/AboutPage'
 import Gallery from '../Gallery/Gallery'
 import CarouselHome from './CarouselHome'
+import Introduction from './Introduction'
 
 export class HomePage extends Component {
     render() {
         return (
             <div>
             <CarouselHome/>
+            <Introduction/>
             <Gallery/>
             <AboutPage/>
             </div>
